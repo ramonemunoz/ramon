b29fd2bf247942982e6b3847c0a613592473b937
Ramon Munoz <ramonmunoz1027@gmail.com>
2020-12-12 16:52:22 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+console.log(`
+
+Ramon Munoz - FrontEnd Engineer
+
+A seasoned front-end developer and web team leader. 
+Excels at coding, testing, launching, and maintaining 
+secure web platforms that deliver on expectations and
+facilitate growth for their organizations. A savvy communicator
+and experienced project manager who enjoys working directly with
+clients, designers, executives, and team members to push the
+envelope and pursue cutting-edge results.
+
+Github : https://github.com/ramonemunoz
+Site : https://ramon-munoz.com/
+
+`);
